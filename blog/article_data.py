ac4ca6e64a2a549478a3f239bbc817e37c203679
Chris Lombardi <125087317+Chrisl2324@users.noreply.github.com>
2023-06-20 16:45:22 -0400

    Add files via upload

---
+++ blog/article_data.py
@@ -0,0 +1,27 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'First Article',
+            'body': 'Hello this is the first sample article.',
+            'author': 'Chris Lombardi',
+            'date created': '06/20/2023',
+        },
+        {
+            'id': 2,
+            'title': 'Second Article',
+            'body': 'This is the second sample article.',
+            'author': 'John Smith',
+            'date created': '06/20/2023',
+        },
+        {
+            'id': 3,
+            'title': 'Third Article',
+            'body': 'Third article body',
+            'author': 'John Doe',
+            'date created': '06/20/23',
+            
+        }
+        
+    ]
+    return articles
